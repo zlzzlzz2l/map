f368289e45d7cc00bc75e88c4245fc1bcf0824a0
zlzzlzz2l <zlzzlzz2l@naver.com>
2022-12-29 15:42:49 +0900

    Config #2 - 프로젝트 초기 세팅

---
+++ src/main/java/com/lab/MapView/MapViewApplication.java
@@ -0,0 +1,13 @@
+package com.lab.MapView;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/MapView/MapViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.MapView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
